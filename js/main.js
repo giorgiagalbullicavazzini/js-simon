a446fbfa05fdeb64c958ff3c98e7fb02fff9f328
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-06 15:56:57 +0100

    5 random numbers array generation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,58 @@
 'use strict';
+
+////////////
+// Functions
+////////////
+
+// A function to create a random number array generator
+function randomArray(arrayElements, min, max) {
+  // Creation of an empty array
+  const generatedArray = [];
+
+  // The array will be filled by a while loop
+  // The loop will generate some random numbers between a min and a max number
+  while (generatedArray.length < arrayElements) {
+      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+      // In order to avoid duplicate numbers, the loop will also check if the randomized number is inside the array or not
+      if (!generatedArray.includes(randomNumber)) {
+          generatedArray.push(randomNumber);
+      }
+  }
+
+  return generatedArray;
+}
+
+// A function to add a class to an element
+function addClass(element, className) {
+  element.classList.add(className);
+}
+
+////////////
+// Main
+////////////
+
+// With the click of a button, the user can generate 5 random numbers
+const play = document.querySelector('.play');
+const randomNumbers = 5;
+
+play.addEventListener('click', function() {
+  const randomSequence = randomArray(randomNumbers, 1, 100);
+  console.log(randomSequence);
+
+  // After the click of the button, the button disappears
+  addClass(play, 'hidden');
+})
+
+
+// When the array is shown on the user screen, a timer starts
+// With a setTimeout function, we can then hide the numbers after 30 seconds
+// After 1 more second, 5 prompt appears
+// The prompts ask the user to type the array numbers in the correct order
+// IF the prompt value is not a number, the prompt appears again
+// To check if the user values are the same of the ones contained in the array, we can use another while loop
+// This loop creates another array and contains the 5 prompts
+// The loop ends when this array contains exactly 5 numbers
+// With a for loop we can then check if the arrays are equal or not
+// At the end of the game, the software generates the results
+// The results contain the correct string and the number of correct prompts
