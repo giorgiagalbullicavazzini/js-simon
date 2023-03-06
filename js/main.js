327e7d138cb343e6243ad723848db9ff5c66cb1e
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-06 23:41:25 +0100

    style

--- js/main.js
+++ js/main.js
@@ -39,9 +39,9 @@ function equalArrays (value, array1, array2) {
 
   for (let i = 0; i < value; i++) {
     if (array1[i] !== array2[i]) {
-      result.push('No');
+      result.push(`<i class="fa-solid fa-xmark"></i>`);
     } else {
-      result.push('Sì');
+      result.push(`<i class="fa-solid fa-check"></i>`);
     }
   }
 
@@ -62,11 +62,14 @@ play.addEventListener('click', function() {
   addClass(play, 'hidden');
 
   // With the click of a button, the user can generate 5 random numbers
-  const randomSequence = randomArray(randomNumbers, 1, 100);
+  const randomSequence = randomArray(randomNumbers, 10, 99);
 
   // When the array is shown on the user screen, a timer starts
   // After some seconds, the array disappears
-  numbersContainer.innerText = randomSequence;
+  for (let i = 0; i < randomSequence.length; i++) {
+    numbersContainer.innerHTML += `<span>${randomSequence[i]}</span> `;
+  }
+
   setTimeout(function() {
     addClass(numbersContainer, 'hidden');
   }, timer * 1000);
@@ -94,7 +97,7 @@ play.addEventListener('click', function() {
     // With another loop we can count the user points
     let points = 0;
     for (let i = 0; i < correctAnswers.length; i++) {
-      if (correctAnswers[i] === 'Sì') {
+      if (correctAnswers[i] === `<i class="fa-solid fa-check"></i>`) {
         points += 1;
       }
     }
@@ -102,20 +105,29 @@ play.addEventListener('click', function() {
     removeClass(numbersContainer, 'hidden');
     
     const inputsContainer = document.querySelector('.user-inputs');
-    inputsContainer.innerText = userSequence;
+    for (let i = 0; i < userSequence.length; i++) {
+      inputsContainer.innerHTML += `<span>${userSequence[i]}</span> `;
+    } 
 
     const resultsContainer = document.querySelector('.results');
-    resultsContainer.innerText = correctAnswers;
+    for (let i = 0; i < correctAnswers.length; i++) {
+      if (correctAnswers[i].includes(`<i class="fa-solid fa-xmark"></i>`)) {
+        resultsContainer.innerHTML += `<span class="red">${correctAnswers[i]}</span> `;
+      } else {
+        resultsContainer.innerHTML += `<span class="green">${correctAnswers[i]}</span> `;
+      }
+    }
 
     const winCondition = document.querySelector('.win-condition');
 
     // At the end of the game, the software generates the results
-    if (correctAnswers.includes('No') && points === 0) {
+    if (correctAnswers.includes(`<i class="fa-solid fa-xmark"></i>`) && points === 0) {
       winCondition.innerText = 'Hai totalizzato 0 punti, ma puoi sempre riprovare!';
-    } else if (correctAnswers.includes('No')) {
+    } else if (correctAnswers.includes(`<i class="fa-solid fa-xmark"></i>`)) {
       winCondition.innerText = `Hai perso, ma hai comunque totalizzato un punteggio di ${points}. Riprova!`;
     } else {
       winCondition.innerText = 'Complimenti, hai vinto! Sei un vero pro player!';
     }
+
   }, (timer + 1) * 1000);
 })
